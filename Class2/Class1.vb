a68ebc03435206fdd4948e1df8131ceba75a1ebf
kivolo <kivolo@ICT-50128K>
2022-01-25 14:07:18 +0200

    implements

--- Class2/Class1.vb
+++ Class2/Class1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Class1
+    Implements EsimenePraktikum
 
+    Const KM2MI = 1.609344
+
+    Private millid As Double = 0
+
+    Public Function teisendaKilomeetridMiilideks(km As Double) As Double Implements 
+        teisendaKmMi(km)
+        Return millid
+    End Function
+    Private Sub teisendaKmMi(ByVal km As Double)
+        millid = km / KM2MI
+    End Sub
 End Class
